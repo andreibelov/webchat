a504865f7381f8fe9f69603094fd3025683eb229
Andrei Belov <andrei_belov@epam.com>
2019-11-02 01:15:09 +0300

    Initial commit

---
+++ backend/src/main/java/net/bootlab/webchat/configs/Endpoints.java
@@ -0,0 +1,17 @@
+package net.bootlab.webchat.configs;
+
+public interface Endpoints {
+
+    String API_PREFIX = "/api/v2/";
+
+    String H2_CONSOLE_URL = "/h2-console";
+
+    String SWAGGER_URL = "/swagger-ui.html";
+
+    String SWAGGER_RESOURCES = "/swagger-resources/**";
+
+    String API_USERS = API_PREFIX + "users";
+
+    String API_STUFF = "/stuff";
+
+}
---
+++ backend/src/main/java/net/bootlab/webchat/controllers/StuffController.java
@@ -0,0 +1,17 @@
+package net.bootlab.webchat.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import static net.bootlab.webchat.configs.Endpoints.API_STUFF;
+
+@Controller
+public class StuffController {
+
+    @RequestMapping(API_STUFF)
+    @ResponseBody
+    public String greetings() {
+        return "greetings.txt";
+    }
+}
